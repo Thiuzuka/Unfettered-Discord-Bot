338d2ee948a571255f9914142e3ae2bcc85bdf72
Vincent Pascua <vinzp_2007@yahoo.com>
2022-09-28 07:44:55 -0600

    Second Commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const fs = require('node:fs');
+const path = require('node:path');
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+/* Slash Commands
+// You should first check if an interaction is a chat input
+// command via .isChatInputCommand(), and then check the .commandName
+// roperty to know which command it is. You can respond to
+// interactions with .reply().
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}');
+	} else if (commandName === 'user') {
+		await interaction.reply('Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}');
+	}
+});
+*/
+
+// Login to Discord with your client's token
+client.login(token);
